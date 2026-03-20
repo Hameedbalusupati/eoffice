63b743f2b28d4978a09a46168c6e015090571de3
Hameedbalusupati <hameedbalusuapti023@gmail.com>
2026-03-20 20:24:05 +0530

    added the db

--- backend/routes/reportRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const ctrl = require("../controllers/reportController");
-const auth = require("../middleware/authMiddleware");
-
-router.use(auth);
-
-router.get("/", ctrl.getReports);
-
-module.exports = router;
